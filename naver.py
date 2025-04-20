c4912fe9dfee2ccb45c35add77166c180a0e082a
jaehoon3123213 <boxstar120@gmail.com>
2025-04-20 10:45:20 +0900

    Update naver.py

--- naver.py
+++ naver.py
@@ -250,6 +250,6 @@ def kakao_shop():
             pass
 
     save_csv(shop_name, stars, addresses,categories)
-naver_shop()
-cal_distance("shop_naver.csv","shop_distance.csv")
+#naver_shop()
+#cal_distance("shop_naver.csv","shop_distance.csv")
 kakao_shop()
